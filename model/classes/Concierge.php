93cae0f3b93414f8e4783dea6a11e739ee24218e
WalterJonas <jonasdowalter@gmail.com>
2023-04-17 17:11:53 -0400

    Registro de entrada :construction:

--- model/classes/Concierge.php
+++ model/classes/Concierge.php
@@ -5,26 +5,24 @@ class Concierge
 	private $username;
 	private $password;
 
-	public function login($username, $password) //Email e senha por parametro
+	public function login($username, $password) 
 	{
 		global $pdo;
-		$sql = $pdo->prepare("SELECT id FROM concierge WHERE username = :u AND password = :p"); //Comando sql
+		$sql = $pdo->prepare("SELECT id FROM concierge WHERE username = :u AND password = :p"); 
 
-		//Substitui as variáveis
 		$sql->bindValue(":u", $username, PDO::PARAM_STR);
 		$sql->bindValue(":p", $password, PDO::PARAM_STR);
-		$sql->execute(); //Executa o comando o sql e retorna alguma coisa
-		if($sql->rowCount()>0) //Conta a quantidade de linhas que retornou do BD (Se retornar algo ele vai logar)
+		$sql->execute(); 
+		if($sql->rowCount()>0) 
 		{
-			//entrar no sistema (sessão)
 			//$dado=$sql->fetch(); //Transforma os dados que retornaram do BD em um vetor
 			//session_start(); //Inicia uma sessão
 			//$_SESSION['id']=$dado['id']; //Inicia sessão com ID_Cliente
-			return true; //logado com sucesso
+			return true;
 		}
 		else
 		{
-			return false; //nao foi possivel logar (Não achou o email e senha no BD)
+			return false; 
 		}
 	}
 
