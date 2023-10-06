7d8b61ea1243b7c0cf8ceedf35d095c2a27d2612
Anton <88747866+AntonyGuzma@users.noreply.github.com>
2023-10-06 16:21:33 -0400

    Tela Login v2 :lipstick: feat

--- index.php
+++ index.php
@@ -11,36 +11,58 @@
 
 </head>
 <body>
-<div class="container">
-    <div class="row">
-      <div class="col-md-6 offset-md-3">
-        <div class="card my-5">
+  <div class="main-login">
+    <div class="left-login">
+      <img class="left-image" src="./view/img/Forgot password-rafiki.png" alt="tela-login">
+    </div>
+    <div class="rigth-login">
+        <h1>Lab-Controller</h1>
+        <form  class="card-login" action="model/buttonActions.php" method="POST">
+          <h1>LOGIN</h1>
+          <div class="textfield">
+            <input type="text" name="username" aria-describedby="emailHelp"
+                placeholder="Usuário" required>
+          </div>
+          <div class="textfield">
+            <input type="password" name="password" placeholder="Senha" required>
+          </div>
+          <button class="btn-login" type="submit" name="login">Login</button>
+        </form>
+    </div>
+  </div>
 
-          <form action="model/buttonActions.php" method="POST" class="card-body cardbody-color p-lg-3">
 
-            <div class="text-center">
-              <img src="./view/img/Login-amico.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
-                width="200px" alt="profile">
-            </div>
 
-            <div class="mb-3">
-              <input type="text" class="form-control" name="username" aria-describedby="emailHelp"
+
+
+
+
+
+
+
+
+
+
+
+  <!-- <div>
+      <div>
+        <div>
+          <form action="model/buttonActions.php" method="POST">
+            <div>
+              <input type="text" name="username" aria-describedby="emailHelp"
                 placeholder="Usuário" required>
             </div>
-            <div class="mb-3">
-              <input type="password" class="form-control" name="password" placeholder="Senha" required>
+            <div>
+              <input type="password" name="password" placeholder="Senha" required>
             </div>
-            <div class="text-center"><button type="submit" name="login" class="btn btn-color px-5 mb-5 w-100">Login</button></div>
-            <div id="emailHelp" class="form-text text-center mb-5 text-dark">Not
-              Registered? <a href="#" class="text-dark fw-bold"> Create an
-                Account</a>
+            <div>
+              <button type="submit" name="login">Login</button>
             </div> 
           </form>
         </div>
 
       </div>
-    </div>
-  </div>
+  </div> -->
 <!--    <form action="model/buttonActions.php" method="POST">
         <input type="text" name="username" placeholder="Usuário" required>
         <input type="password" name="password" placeholder="Senha" required>
